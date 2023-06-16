5ff3990cb24c7b373d1be55cfb7cb3d9dc9d2ae9
Lucy Kihuha <nyawiralucy104@gmail.com>
2023-06-16 22:26:49 +0300

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
- /**
-  * main - Entry point 
-  * Description: program that prints the alphabet in lowercase
-  * Return: 0 (Success)
-  */
- int main(void)
- {      
+/**
+ * main - Entry point
+ * Description: program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
 	char ch = 'z';
 
 	while (ch >= 'a')
@@ -15,6 +15,5 @@
 		ch--;
 	}
 	 putchar('\n');
-
-	 return (0);
+	return (0);
 }
