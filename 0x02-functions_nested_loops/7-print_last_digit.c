e916e0e6eb30e8ae81dc03a23bb5a83bc4e0ae97
Lucy Kihuha <nyawiralucy104@gmail.com>
2023-06-21 01:37:02 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,12 @@ int print_last_digit(int i)
 	int last_digit;
 
 	if (i < 0)
+	{
 		i = i * -1;
+		last_digit = i % 10;
+		_putchar('0' + last_digit);
+		return (last_digit);
+	}
 
 	last_digit = i % 10;
 	_putchar('0' + last_digit);
