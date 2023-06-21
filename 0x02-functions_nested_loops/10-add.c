4cb0a3ed3bf58546f0e06fdc02526b1d5a21f140
Lucy Kihuha <nyawiralucy104@gmail.com>
2023-06-21 16:40:22 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * add - check the code
+  * @l: first parameter
+  * @n: second parameter
+  * Return: 0 (Success)
+  */
+int add(int l, int n)
+{
+	return (l + n);
+}
