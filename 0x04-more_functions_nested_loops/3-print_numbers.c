9adf4cbaf4088634db6a6a09d59ebce61efc54e8
Lucy Kihuha <nyawiralucy104@gmail.com>
2023-06-22 12:33:19 +0300

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_numbers - check the code
+  * @l: parameter
+  * Return: 0 (Success)
+  */
+void print_numbers(void)
+{
+	int l;
+
+	for (l = 48; l < 58; l++)
+	{
+		_putchar(l);
+	}
+
+	_putchar('\n');
+}
