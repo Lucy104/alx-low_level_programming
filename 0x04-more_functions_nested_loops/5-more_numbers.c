76b3fffcc156f284456229326d95ba0740b747b5
Lucy Kihuha <nyawiralucy104@gmail.com>
2023-06-22 13:24:23 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * more_numbers - check the code
+  * Return: 0 (Success)
+  */
+void more_numbers(void)
+{
+	int z, k;
+
+	for (z = 0; z < 14; z++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			if (k > 9)
+				_putchar(k / 10 + '0');
+			_putchar('0' + k % 10);
+		}
+		_putchar('\n');
+	}
+}
+
