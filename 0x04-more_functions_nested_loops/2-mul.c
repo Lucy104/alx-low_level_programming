60a02de1db5414d86d20684005874d20fdaa56d8
Lucy Kihuha <nyawiralucy104@gmail.com>
2023-06-22 09:56:21 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * mul - check the code
+  * @a: first parameter
+  * @b: second parameter
+  * Return: 0 (Success)
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
