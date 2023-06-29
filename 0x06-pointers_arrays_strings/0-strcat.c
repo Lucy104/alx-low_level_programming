7c9da045362929fe41056e1ffe83b481c2414fa9
Lucy Nyawira <nyawiralucy104@gmail.com>
2023-06-29 14:21:57 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  * *_strcat - function that concatenates two strings
+  * @*dest: first string
+  * @*src: second string
+  * Return: 
+  */
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
