8a044d5c820d543cc8ab3b72b8f21580cf9bf885
Lucy Nyawira <nyawiralucy104@gmail.com>
2023-06-30 21:21:58 +0300

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  int a[] = {1, 2, 3, 4, 5};
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
