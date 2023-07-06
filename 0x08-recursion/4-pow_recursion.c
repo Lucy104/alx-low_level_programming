8e49a7a7e3e975b4a7621a654acf3fd480f773a9
Lucy Nyawira <nyawiralucy104@gmail.com>
2023-07-07 00:18:04 +0300

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
   * _pow_recursion - function that returns the value of x raised to power y
   * @x: input
   * @y: input
-  * Return: Alwaysn0
+  * Return: Always 0
   */
 int _pow_recursion(int x, int y)
 {
@@ -16,6 +16,10 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 	{
 		return (0);
+	}
+	else if (y == 1)
+	{
+		return (x);
 	}
 	return (x * _pow_recursion(x, y - 1));
 }
