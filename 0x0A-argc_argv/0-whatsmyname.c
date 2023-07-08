1837c7d523a2d5de825956db9fff05f677168a7c
Lucy Nyawira <nyawiralucy104@gmail.com>
2023-07-08 18:27:38 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - Entry point
+  * @argc: count of argument
+  * @argv: arry of pointer to a string
+  *
+  * Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_Hi
+#define MAIN_H
+
+int _putchar(char c);
+#endif
