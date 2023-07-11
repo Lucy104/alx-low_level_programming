79ec5eaff65acdb874dc1b0f6333edf4a1d2ff92
Lucy Nyawira <nyawiralucy104@gmail.com>
2023-07-11 14:03:03 +0300

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * create_array - creates an array of chars
+  * @size: the size of the memory to print
+  * @c: parameter
+  * Return: Always 0
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int l;
+	char *lArray;
+
+	for (l = 0; l < size; l++)
+	{
+		if (size == 0)
+			return (NULL);
+	}
+
+	/* return a pointer to the allocated memory */
+	lArray = malloc(size * sizeof(char));
+
+	if (lArray == NULL)
+		return (NULL);
+	for (l = 0; l < size; l++)
+	{
+		lArray[l] = c;
+	}
+	return (lArray);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+# 0x0B. C - malloc, free
+0. function that creates an array of chars, and initializes it with a char
+1. function that returns a pointer to a newly allocated space in memory
+2. function that concatenates two strings.
+3. function that returns a pointer to a 2 dimensional array of integer
+4. function that frees a 2 dimensional grid
+5. 
